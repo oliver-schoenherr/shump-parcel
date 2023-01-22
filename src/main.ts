b11ff7f2787652502249eddad7432463341fd929
os191n <oschoenherr@wayfair.com>
2023-01-22 17:34:12 +0100

    fix debug

--- src/main.ts
+++ src/main.ts
@@ -2,18 +2,16 @@ import {Color, Engine} from 'excalibur'
 import {loader} from "./resources";
 import {Level} from "./level";
 
-export function start() {
-    const game = new Engine({
-        width: 800,
-        height: 600,
-        antialiasing: false,
-        backgroundColor: Color.Black
-    });
+const game = new Engine({
+    width: 800,
+    height: 600,
+    antialiasing: false,
+    backgroundColor: Color.Black
+});
 
-    game.start(loader).then(() => {
-        const level = new Level();
-        game.addScene("main", level);
-        game.goToScene("main");
-    })
-}
+game.start(loader).then(() => {
+    const level = new Level();
+    game.addScene("main", level);
+    game.goToScene("main");
+})
 
